e18bc4fb3f27357362fe67bedc88fb327a2ac302
LordKakabel <lord.kakabel@gmail.com>
2021-03-26 13:50:56 -0400

    Can fire projectiles with a cooldown.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float _speed = 8f;
+    [SerializeField] private float _yBoundary = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y > _yBoundary)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,24 +8,34 @@ public class Player : MonoBehaviour
     [SerializeField] private float _xBoundary = 9f;
     [SerializeField] private float _yTopBoundary = 0f;
     [SerializeField] private float _yBottomBoundary = -3.5f;
+    [SerializeField] private Transform _pfProjectile = null;
+    [SerializeField] private Vector3 _projectileOffset = new Vector3(0, 0.75f, 0);
+    [SerializeField] private float _fireRate = 0.5f;
+
+    private float _nextFire = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         // Set the current position to new position (0, 0, 0)
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFire)
+        {
+            FireProjectile();
+        }
     }
 
     void CalculateMovement() 
     {
-        // Time.deltaTime = 1 second
-        // transform.Translate(Vector3.right * 5 * Time.deltaTime) will move 5 m per second, independent of framerate
+        //! Think of Time.deltaTime as 1 second
+        //! transform.Translate(Vector3.right * 5 * Time.deltaTime) will move 5 m per second, independent of framerate
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -54,5 +64,12 @@ public class Player : MonoBehaviour
                 transform.position.y,
                 transform.position.z);
         }
+    }
+
+    private void FireProjectile() {
+        // Reset the cooldown timer
+        _nextFire = Time.time + _fireRate;
+
+        Instantiate(_pfProjectile, transform.position + _projectileOffset, Quaternion.identity);
     }
 }
