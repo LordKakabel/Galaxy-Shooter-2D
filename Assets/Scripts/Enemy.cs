cbbde69c425000b2f7ce18c77c20cd7b2d21e688
LordKakabel <lord.kakabel@gmail.com>
2021-03-27 13:30:03 -0400

    Enemies and player damage.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+    [SerializeField] private float _yRange = 7.5f;
+    [SerializeField] private float _xRange = 9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        // Respawn at top of screen with a random x position
+        if (transform.position.y < -_yRange)
+        {
+            transform.position = new Vector3(
+                Random.Range(-_xRange, _xRange),
+                _yRange,
+                transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Hit by " + other.name);
+
+        if (other.tag == "Laser") {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        else if (other.tag == "Player") {
+            /*Player player = other.transform.GetComponent<Player>();
+            if (player != null) {
+                player.Damage();
+            }*/
+            other.transform.GetComponent<Player>().Damage();
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     [SerializeField] private Transform _pfProjectile = null;
     [SerializeField] private Vector3 _projectileOffset = new Vector3(0, 0.75f, 0);
     [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] private int _lives = 3;
 
     private float _nextFire = 0f;
 
@@ -71,5 +72,19 @@ public class Player : MonoBehaviour
         _nextFire = Time.time + _fireRate;
 
         Instantiate(_pfProjectile, transform.position + _projectileOffset, Quaternion.identity);
+    }
+
+    public void Damage() {
+        _lives--;
+
+        if (_lives <= 0) {
+            GameOver();
+        }
+    }
+
+    private void GameOver() 
+    {
+        Destroy(gameObject);
+        Debug.Log("Game Over");
     }
 }
