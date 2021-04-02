7292bb18eb1537682d89c01a320d4d8dce92bc81
LordKakabel <lord.kakabel@gmail.com>
2021-04-01 23:20:07 -0400

    Main Menu screen.

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    private bool _isGameOver = false;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +14,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) && _isGameOver) {
-            RestartGame();
-        }
+        
     }
 
-    public void GameOver() {
-        _isGameOver = true;
-    }
-
-    private void RestartGame() {
+    public void LoadGame() {
         SceneManager.LoadScene(1);
     }
 }
