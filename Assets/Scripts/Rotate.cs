7e74d7742713bea8977e98253a8c571c70da97e2
LordKakabel <lord.kakabel@gmail.com>
2021-04-10 01:41:10 -0400

    Ammo Collectable with graphics.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 5f;
+    [SerializeField] private float _scalingRange = 0.2f;
+    [SerializeField] private float _scalingSpeed = 4f;
+
+    private Vector3 _originalXScale;
+
+    private void Awake()
+    {
+        _originalXScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * _rotationSpeed * Time.deltaTime);
+
+        transform.localScale = new Vector3(
+            _originalXScale.x + (Mathf.Sin(Time.time * _scalingSpeed) * _scalingRange),
+            _originalXScale.y,
+            _originalXScale.z);
+    }
+}
