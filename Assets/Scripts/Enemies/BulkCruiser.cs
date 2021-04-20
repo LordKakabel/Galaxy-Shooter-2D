a82666b8aa74af0a2c56fb6986e3fd629c7f5018
LordKakabel <lord.kakabel@gmail.com>
2021-04-19 22:57:13 -0400

    Aggressive Enemy Type part 1 - Bulk Cruiser can detect when the player is near.

---
+++ Assets/Scripts/Enemies/BulkCruiser.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulkCruiser : Enemy
+{
+    [SerializeField] private LayerMask _layerMask = 0;
+    [SerializeField] private float _detectionRadius = 3f;
+    [SerializeField] private float _rammingSpeed = 7f;
+    [SerializeField] private float _xBoundary = 15f;
+
+    private enum State { Cruising, Tracking, Ramming };
+    private State _state = State.Cruising;
+    private Vector3 _playersDetectedPosition;
+
+    protected override void CalculateMovement()
+    {
+        switch (_state)
+        {
+            case State.Cruising:
+                if (!DetectPlayer())
+                {
+                    base.CalculateMovement();
+                }
+                break;
+            case State.Tracking:
+                AlignWithPlayer();
+                break;
+            case State.Ramming:
+                Ram();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private bool DetectPlayer()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(transform.position, _detectionRadius, _layerMask);
+        if (collider)
+        {
+            _playersDetectedPosition = collider.transform.position;
+            _state = State.Tracking;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void AlignWithPlayer()
+    {
+        //? Rotate along z-axis (using _speed) toward the position where the player was detected
+        //? until a forward-facing ray hits the player, then switch to Ramming
+    }
+
+    private void Ram()
+    {
+        transform.Translate(Vector3.down * _rammingSpeed * Time.deltaTime);
+
+        // If out of bounds, reset roation, switch to Cruising, and respawn
+        if (Mathf.Abs(transform.position.x) >= _xBoundary || Mathf.Abs(transform.position.y) >= _ySpawnPoint)
+        {
+            transform.rotation = Quaternion.identity;
+            _state = State.Cruising;
+            SpawnLocation();
+        }
+    }
+}
